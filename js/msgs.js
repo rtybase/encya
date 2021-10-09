e154fccb114bc143b9d7c6cf540c73a14aeeab2b
rtybase <ruslan.ciurca@gmail.com>
2021-10-09 18:38:43 +0100

    Removed FB and Snapchat icons, added sms and email support instead

--- js/msgs.js
+++ js/msgs.js
@@ -3,8 +3,8 @@ MSG_SCHEMA['telegram'] = 'tg://msg_url?text=<msg>';
 MSG_SCHEMA['viber'] = 'viber://forward?text=<msg>';
 MSG_SCHEMA['twitter'] = 'twitter://post?message=<msg>';
 MSG_SCHEMA['whatsapp'] = 'whatsapp://send?text=<msg>';
-MSG_SCHEMA['snapchat'] = '';
-MSG_SCHEMA['facebook'] = '';
+MSG_SCHEMA['sms'] = 'sms:?body=<msg>';
+MSG_SCHEMA['email'] = 'mailto:?subject=encya-msg&body=<msg>';
 
 function postMessage(messanger, message) {
 let schema = MSG_SCHEMA[messanger.toLowerCase()];
